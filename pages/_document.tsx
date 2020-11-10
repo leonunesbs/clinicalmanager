17b456ff61fd8c3d33aa09e48b3b16a53a374233
leonunesbs <leonunesbs@gmail.com>
2020-11-10 01:43:02 -0300

    favicon meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,24 @@ class MyDocument extends Document<DocumentProps> {
             name="google-site-verification"
             content="wisnFadn82Ndz9TMStMgCiRpbFSgVe4WmNl-kJHc1Uo"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href={`${require('../public/icons/apple-touch-icon.png?webp')}`}
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href={`${require('../public/icons/favicon-32x32.png?webp')}`}
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href={`${require('../public/icons/favicon-16x16.png?webp')}`}
+          />
+          <link rel="manifest" href="/icons/site.webmanifest" />
         </Head>
         <body>
           <Main />
