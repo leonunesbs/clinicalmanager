50b9017337196512d7ecb77312ca77ac74f7e8ae
leonunesbs <leonunesbs@gmail.com>
2020-11-14 14:51:43 -0300

    adjust size

--- components/ButtonWithIcon/index.tsx
+++ components/ButtonWithIcon/index.tsx
@@ -29,7 +29,7 @@ const ButtonWithIcon: React.ForwardRefRenderFunction<
       p={1}
       {...rest}
     >
-      <Flex as={icon} size="xs" />
+      <Flex as={icon} size="2xs" />
       {rightText && (
         <Text fontWeight="light" ml={1}>
           {rightText}
