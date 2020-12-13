0db5237eb81169ea1565da7e999b0b97456f4c73
leonunesbs <leonunesbs@gmail.com>
2020-12-13 09:09:51 -0300

    rework painel menu

--- components/PainelMenu/index.tsx
+++ components/PainelMenu/index.tsx
@@ -1,22 +1,18 @@
+/* eslint-disable multiline-ternary */
 // eslint-disable-next-line no-use-before-define
-import React, { useCallback, useRef } from 'react'
-import {
-  Button,
-  Drawer,
-  Text,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerHeader,
-  DrawerOverlay,
-  Flex,
-  useDisclosure
-} from '@chakra-ui/core'
+import React, { useCallback, useState } from 'react'
+import { Button, Flex, Stack, Text } from '@chakra-ui/core'
 import { MdHome, MdPerson } from 'react-icons/md'
+import {
+  BsChevronCompactRight,
+  BsCalendar,
+  BsChevronCompactLeft,
+  BsChevronCompactUp,
+  BsChevronCompactDown
+} from 'react-icons/bs'
 import { useRouter } from 'next/router'
-import { FiMenu } from 'react-icons/fi'
-import { BsCalendar } from 'react-icons/bs'
+
 import { FaFileArchive } from 'react-icons/fa'
-import MenuItem from '../Header/MenuItem'
 
 interface PainelMenuProps {
   painelMenuView: string | string[]
@@ -35,7 +31,7 @@ const PainelMenu: React.FC<PainelMenuProps> = ({ painelMenuView }) => {
       title: 'Calendário',
       href: '/painel?d=calendario',
       icon: BsCalendar,
-      slug: ['calendário']
+      slug: ['calendario']
     },
     {
       title: 'Pacientes',
@@ -51,9 +47,7 @@ const PainelMenu: React.FC<PainelMenuProps> = ({ painelMenuView }) => {
       slug: ['prontuarios']
     }
   ]
-
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const btnRef = useRef<HTMLDivElement>(null)
+  const [isExtended, setIsExtended] = useState(true)
 
   const checkIsActive = useCallback(
     (slug: string | string[]) => {
@@ -69,87 +63,111 @@ const PainelMenu: React.FC<PainelMenuProps> = ({ painelMenuView }) => {
 
   return (
     <>
-      <Flex
-        p={1}
-        pl="5%"
-        d={['flex', 'none']}
-        shadow="md"
-        backgroundColor="blue.100"
-      >
-        <Button
-          backgroundColor=""
-          color={'blue.700'}
-          _hover={{ backgroundColor: 'blue.400' }}
-          _active={{ backgroundColor: 'blue.100', color: 'blue.700' }}
-          onClick={onOpen}
-          textAlign="center"
-          p={0}
+      {/* Side menu Desktop */}
+      <Flex d={['none', 'flex']}>
+        <Stack flexGrow={1} p={2}>
+          {menuItems.map(item => {
+            const Icon = item.icon
+            return (
+              <Button
+                key={item.title}
+                display="flex"
+                variant="solid"
+                minW={isExtended && '160px'}
+                borderRadius="sm"
+                justifyContent="flex-start"
+                backgroundColor="blue.100"
+                onClick={() => {
+                  router.push(item.href)
+                }}
+                _hover={{ backgroundColor: 'blue.200' }}
+                _active={{
+                  backgroundColor: 'blue.500',
+                  color: 'blue.100'
+                }}
+                isActive={checkIsActive(item.slug)}
+              >
+                {isExtended ? (
+                  <>
+                    <Icon size={18} />
+                    <Text ml={4}>{item.title}</Text>
+                  </>
+                ) : (
+                  <Icon size={18} />
+                )}
+              </Button>
+            )
+          })}
+        </Stack>
+        <Flex
+          align="center"
+          shadow="md"
+          justify="center"
+          cursor="pointer"
+          backgroundColor="blue.400"
+          borderRightColor="blue.700"
+          borderRightWidth="1px"
+          onClick={() => setIsExtended(!isExtended)}
         >
-          <Flex as={FiMenu} size="2xs" />
-        </Button>
-        <Drawer
-          isOpen={isOpen}
-          placement="left"
-          onClose={onClose}
-          size="xs"
-          finalFocusRef={btnRef}
-        >
-          <DrawerOverlay>
-            <DrawerContent>
-              <DrawerCloseButton borderRadius="sm" />
-              <DrawerHeader>Menu</DrawerHeader>
-
-              <Flex flexDirection="column" align="center" flexGrow={1}>
-                {menuItems.map(item => (
-                  <MenuItem key={item.title} href={item.href} w="80%">
-                    <Text fontSize="sm" textAlign="center">
-                      {item.title}
-                    </Text>
-                  </MenuItem>
-                ))}
-              </Flex>
-            </DrawerContent>
-          </DrawerOverlay>
-        </Drawer>
+          <Flex
+            color="blue.100"
+            size={4}
+            as={isExtended ? BsChevronCompactLeft : BsChevronCompactRight}
+          />
+        </Flex>
       </Flex>
-      <Flex
-        flexGrow={1}
-        h="2px"
-        backgroundColor="blue.400"
-        d={['inital', 'none']}
-      />
-      {/* Side menu */}
-      <Flex
-        width="30%"
-        minWidth="150px"
-        maxWidth="300px"
-        display={['none', 'flex']}
-      >
-        <Flex display="column" width="100%" p={2} mt={4}>
-          {menuItems.map(item => (
-            <Button
-              key={item.title}
-              display="flex"
-              leftIcon={item.icon}
-              variant="solid"
-              width="100%"
-              mb={2}
-              borderRadius="sm"
-              justifyContent="flex-start"
-              backgroundColor="blue.100"
-              onClick={() => {
-                router.push(item.href)
-              }}
-              _hover={{ backgroundColor: 'blue.200' }}
-              _active={{
-                backgroundColor: 'blue.500',
-                color: 'blue.100'
-              }}
-              isActive={checkIsActive(item.slug)}
-            >
-              {item.title}
-            </Button>
-          ))}
+
+      {/* Side menu Mobile */}
+      <Flex d={['flex', 'none']} flexDir="column">
+        <Stack isInline={!isExtended} justify="center" flexGrow={1} p={2}>
+          {menuItems.map(item => {
+            const Icon = item.icon
+            return (
+              <Button
+                key={item.title}
+                display="flex"
+                variant="solid"
+                minW={isExtended && '160px'}
+                borderRadius="sm"
+                justifyContent="flex-start"
+                backgroundColor="blue.100"
+                onClick={() => {
+                  router.push(item.href)
+                }}
+                _hover={{ backgroundColor: 'blue.200' }}
+                _active={{
+                  backgroundColor: 'blue.500',
+                  color: 'blue.100'
+                }}
+                isActive={checkIsActive(item.slug)}
+              >
+                {isExtended ? (
+                  <>
+                    <Icon size={18} />
+                    <Text ml={4}>{item.title}</Text>
+                  </>
+                ) : (
+                  <Icon size={18} />
+                )}
+              </Button>
+            )
+          })}
+        </Stack>
+        <Flex
+          align="center"
+          shadow="md"
+          justify="center"
+          cursor="pointer"
+          backgroundColor="blue.400"
+          borderRightColor="blue.700"
+          borderRightWidth="1px"
+          onClick={() => setIsExtended(!isExtended)}
+        >
+          <Flex
+            color="blue.100"
+            size={4}
+            as={isExtended ? BsChevronCompactUp : BsChevronCompactDown}
+          />
         </Flex>
       </Flex>
     </>
