4672da56d493f3cd74f056d0b6a9ea7ced44a302
leonunesbs <leonunesbs@gmail.com>
2020-12-15 19:29:33 -0300

    fix

--- components/PainelContainer/CalendárioView/index.tsx
+++ components/PainelContainer/CalendárioView/index.tsx
@@ -36,19 +36,6 @@ const CalendárioView: React.FC = () => {
 
   const modalAgendaDisclosure: UseDisclosureReturn = useDisclosure()
 
-  if (!data) {
-    return (
-      <Flex display="column">
-        <Flex justify="space-between" mb={4}>
-          <Heading as="h2" size="lg" color="blue.100">
-            Calendário
-          </Heading>
-          <Skeleton h="500px" />
-        </Flex>
-      </Flex>
-    )
-  }
-
   useEffect(() => {
     if (data) {
       setAgendas(
@@ -143,6 +130,18 @@ const CalendárioView: React.FC = () => {
     [agendas]
   )
 
+  if (!data) {
+    return (
+      <Flex display="column">
+        <Flex justify="space-between" mb={4}>
+          <Heading as="h2" size="lg" color="blue.100">
+            Calendário
+          </Heading>
+          <Skeleton h="500px" />
+        </Flex>
+      </Flex>
+    )
+  }
   return (
     <>
       <Head>
